8c4febba720b11a0fe928bafd674fb2a6c3f4c3c
alaametwale <alaameto86@gmail.com>
2025-12-03 04:04:11 +0200

    Apply all updates: CounterApp useState, ContextAPI, Redux, fix Header.tsx

--- alx-project-0x06/components/layouts/Header.tsx
+++ alx-project-0x06/components/layouts/Header.tsx
@@ -1,16 +1,20 @@
 import Link from "next/link";
 import Button from "../common/Button";
 import { usePathname } from "next/navigation";
-import { useCount } from "@/context/CountContext";
+import { RootState } from "@/store/store";
+import { useSelector } from "react-redux";
 
 const Header: React.FC = () => {
   const pathname = usePathname();
-  const { count } = useCount();
+  const count = useSelector((state: RootState) => state.counter.value);
 
   return (
     <header className="fixed w-full bg-white shadow-md">
       <div className="container mx-auto flex justify-between items-center py-6 px-4 md:px-8">
-        <Link href="/" className="text-3xl md:text-5xl font-bold text-gray-800 tracking-tight">Splash App</Link>
+        <Link href="/" className="text-3xl md:text-5xl font-bold text-gray-800 tracking-tight">
+          Splash App
+        </Link>
+
         <div className="flex gap-4">
           {!["/counter-app"].includes(pathname) ? (
             <>
